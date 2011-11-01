59e3d404ffa59c973d163253ffd137bd21553779
Steven Boles <boles.steven@gmail.com>
2011-11-01 14:12:00 -0400

    adding Meal scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 MealPlanner::Application.routes.draw do
+  resources :meals
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
---
+++ db/migrate/20111101180916_create_meals.rb
@@ -0,0 +1,10 @@
+class CreateMeals < ActiveRecord::Migration
+  def change
+    create_table :meals do |t|
+      t.string :title
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
