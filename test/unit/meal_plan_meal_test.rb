f7b9db8eb1a3eca1a68dd3c106bdedf37b2c4129
Steven Boles <boles.steven@gmail.com>
2011-11-01 15:08:25 -0400

    adding a fixture and a simple test

--- test/unit/meal_plan_meal_test.rb
+++ test/unit/meal_plan_meal_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class MealPlanMealTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "has one meal" do
+    mpm = MealPlanMeal.where(custom_description:"Test")
+    assert_not_nil mpm
+  end
 end
