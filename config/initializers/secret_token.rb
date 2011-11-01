8014f98dbfd3356075cbc5f4bf67f932f2c13a83
Steven Boles <boles.steven@gmail.com>
2011-11-01 10:49:47 -0400

    initial rails new generation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MealPlanner::Application.config.secret_token = '92a993a90182dcfe65664f5cf84eae802089a8a48db9db76eac127e976df285b06cb161782f406c8e019fb194825621a785f0be5c49b52c0a620544d56dab8d5'
